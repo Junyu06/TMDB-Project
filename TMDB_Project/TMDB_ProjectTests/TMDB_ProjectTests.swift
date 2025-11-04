1c018a06c65c14822ceffc7d7c111ac2090bdfa2
Junyu Li <70296613+Junyu06@users.noreply.github.com>
2025-11-04 15:46:02 -0500

    basic structure

---
+++ TMDB_Project/TMDB_ProjectTests/TMDB_ProjectTests.swift
@@ -0,0 +1,17 @@
+//
+//  TMDB_ProjectTests.swift
+//  TMDB_ProjectTests
+//
+//  Created by Junyu Li on 11/4/25.
+//
+
+import Testing
+@testable import TMDB_Project
+
+struct TMDB_ProjectTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
